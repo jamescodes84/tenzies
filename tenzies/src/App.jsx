1d1bcac75f3381985e4f77b82af0d2e886345b06
jamescodes84 <james.reynolds9@icloud.com>
2025-06-13 10:56:27 -0600

    random numbers can be generated

--- tenzies/src/App.jsx
+++ tenzies/src/App.jsx
@@ -1,16 +1,27 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Die from './components/Die'
 
 
 export default function App() {
+  const [diceValues, setDiceValues] = React.useState([])
  
+  function generateAllNewDice(){
+    let randomValues = []
+    for (let i = 0; i < 10; i++){
+      randomValues.push(Math.ceil(Math.random() * 6))
+    }
+    setDiceValues(randomValues)
+    console.log(diceValues)
+  }
+  generateAllNewDice()
+
   console.log("so it begins...")
   return (
     <>
         <main>
-          <div class="dice-container">
+          <div className="dice-container">
               <Die value="1" /> 
               <Die value= "2" />
               <Die value="3"/>
