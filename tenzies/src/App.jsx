8ef120e240853c3e372e51ab5cde5f89aef8fc15
jamescodes84 <james.reynolds9@icloud.com>
2025-06-13 10:38:49 -0600

    css updated

--- tenzies/src/App.jsx
+++ tenzies/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Die from './components/Die'
 
 
 export default function App() {
@@ -8,7 +9,20 @@ export default function App() {
   console.log("so it begins...")
   return (
     <>
-        <main></main>
+        <main>
+          <div class="dice-container">
+              <Die value="1" /> 
+              <Die value= "2" />
+              <Die value="3"/>
+              <Die value="4"/>
+              <Die value="5" />
+              <Die value="6" />
+              <Die value="7" />
+              <Die value="8" />
+              <Die value="9" />
+              <Die value="10" />
+          </div>
+        </main>
     </>
    
   )
