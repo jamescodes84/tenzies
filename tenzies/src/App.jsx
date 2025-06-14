1baf198f2fde9ec5da7c66c949c202bbe2c3ece3
jamescodes84 <james.reynolds9@icloud.com>
2025-06-14 11:26:46 -0600

    gameWon variable added, game rules added

--- tenzies/src/App.jsx
+++ tenzies/src/App.jsx
@@ -6,7 +6,14 @@ import Die from './components/Die'
 import {nanoid } from 'nanoid'
 export default function App() {
   const [dice, setDice] = React.useState(generateAllNewDice())
-  
+  let gameWon = gameOver()
+
+  if (gameWon){
+    console.log("game is over")
+  }else{
+    console.log("game is not over")
+  }
+
   function generateAllNewDice(){
     return new Array(10)
             .fill(0)
@@ -17,6 +24,61 @@ export default function App() {
             }))
   }
 
+  function gameOver() {
+    // check to see how many dice
+    // have been held
+    let finishedDiceCount = 0
+    let tenDiceHeld = false
+    let tenDiceSame = false
+    let heldValue = null
+    //count how many dice are held
+    dice.map(die => {
+      if (die.isHeld)
+        finishedDiceCount++
+        heldValue = die.value
+    })
+    // if ten are held, ensure the values all match
+    if (finishedDiceCount == 10){
+      let tenDiceHeld = true
+      let matchingDice = 0
+
+     // console.log("ten dice held")
+      dice.map(die => {
+        if (die.value === heldValue){
+          matchingDice++
+        }
+      })
+
+      if (matchingDice === 10){
+        tenDiceSame = true
+        //console.log("ten dice match")
+      }
+
+      if (tenDiceHeld && tenDiceSame){
+        return true
+      }
+
+    } else {
+      return false
+    }
+
+    /**********************
+     * Bob's Logic from the course is below:
+     * 
+     * 
+     * if (
+        dice.every(die => die.isHeld) && 
+        dice.every(die => die.value === dice[0].value)
+    ) {
+        console.log("Game won!")
+    }
+     
+     *************************/
+
+
+  } 
+  
+  
   
   function rollDice(){
      setDice(prevDice => prevDice.map(die => {
@@ -30,6 +92,7 @@ export default function App() {
             }
       }
     }))
+    //gameWon()
   }
 
   function hold(buttonID) {
@@ -50,14 +113,16 @@ export default function App() {
   }
   
   let diceElements = dice.map(num => <Die holdFunc={hold} value={num.value}  id={num.id} key={num.id} isHeld={num.isHeld} />)
-
+ 
   return (
     <>
         <main>
+          <h1 className="title">Tenzies</h1>
+            <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
           <div className="dice-container">
               {diceElements}
           </div>
-          <button onClick={rollDice} className="roll-dice" >Roll Dice</button>
+          <button onClick={rollDice} className="roll-dice" >{gameWon ?"New Game":"Roll Dice"}</button>
         </main>
     </>
    
